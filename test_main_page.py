8a8b1124b34f7dfa6e0d8bd490844bf8cc15e3cb
engrigoreva <engrigoreva@diasoft.ru>
2020-04-26 20:03:30 +0300

    добавление проверки форм log in и регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,3 +12,5 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    login_page = page.go_to_login_form()
+    login_page.should_be_login_page()
